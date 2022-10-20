26cb8c4f52722dcfd6c2d2d268e09d298a52229c
Chamara Dayarathne <ckchamara22@gmail.com>
2022-10-20 11:23:14 +0530

    ep 9 started

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -29,28 +29,28 @@ class Product extends Equatable {
   static List<Product> products = [
     const Product(
         name: 'Soft Drink #1',
-        catagory: 'Soft Drinks',
+        catagory: 'Soft Drink',
         imgUrl: 'https://images.unsplash.com/photo-1543253687-c931c8e01820?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
         price: 2.99,
         isRecommended: true,
         isPopular: false
     ),const Product(
         name: 'Soft Drink #2',
-        catagory: 'Soft Drinks',
+        catagory: 'Soft Drink',
         imgUrl: 'https://images.unsplash.com/photo-1581006852262-e4307cf6283a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
         price: 2.99,
         isRecommended: true,
         isPopular: true
     ),const Product(
         name: 'Soft Drink #3',
-        catagory: 'Soft Drinks',
+        catagory: 'Soft Drink',
         imgUrl: 'https://images.unsplash.com/photo-1579630942078-100a2f8e9052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
         price: 2.99,
         isRecommended: true,
         isPopular: false
     ),const Product(
         name: 'Soft Drink #4',
-        catagory: 'Soft Drinks',
+        catagory: 'Water',
         imgUrl: 'https://images.unsplash.com/photo-1597906336500-757b42d34427?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
         price: 2.99,
         isRecommended: true,
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,83 +1,90 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/product_model.dart';
 
 class ProductCard extends StatelessWidget {
-
   final Product product;
   final double widthFactor;
 
   const ProductCard({
-    Key? key, required this.product,  this.widthFactor = 2.5,
+    Key? key,
+    required this.product,
+    this.widthFactor = 2.5,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final double widthValue = MediaQuery.of(context).size.width / widthFactor;
-    return Stack(
-      children: [
-        Container(
-          height: 150,
-          width: widthValue,
-          child: Image.network(
-            product.imgUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned(
-          top: 60,
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.5,
-            height: 80,
-            decoration:
-            BoxDecoration(color: Colors.black.withAlpha(50)),
-          ),
-        ),
-        Positioned(
-          top: 60,
-          left: 5,
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.5 - 10,
-            height: 70,
-            decoration: BoxDecoration(color: Colors.black),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5
-                              ?.copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          '\$${product.name}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6
-                              ?.copyWith(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      icon: Icon(Icons.add_circle), onPressed: () {}, color: Colors.white,),
-                  )
-                ],
-              ),
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, 'product', arguments: product);
+      },
+      child: Stack(
+        children: [
+          Container(
+            height: 150,
+            width: widthValue,
+            child: Image.network(
+              product.imgUrl,
+              fit: BoxFit.cover,
             ),
           ),
-        )
-      ],
+          Positioned(
+            top: 60,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2.5,
+              height: 80,
+              decoration: BoxDecoration(color: Colors.black.withAlpha(50)),
+            ),
+          ),
+          Positioned(
+            top: 60,
+            left: 5,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2.5 - 10,
+              height: 70,
+              decoration: BoxDecoration(color: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                ?.copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            '\$${product.name}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6
+                                ?.copyWith(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        icon: Icon(Icons.add_circle),
+                        onPressed: () {},
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
